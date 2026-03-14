fdd48b143202b27cd0873b6c3f79d3730a37c275
trung081102 <116638032+trung081102@users.noreply.github.com>
2026-03-14 15:43:52 +0700

    Create Market class for market data representation

---
+++ backend/models/Market.js
@@ -0,0 +1,10 @@
+class Market {
+  constructor(id, question, yesPrice, noPrice) {
+    this.id = id
+    this.question = question
+    this.yesPrice = yesPrice
+    this.noPrice = noPrice
+  }
+}
+
+module.exports = Market
